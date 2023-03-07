14ea5736be0c27a26f23da2eaf9d1d6b3af94dcc
afan2k0 <125404099+afan2k0@users.noreply.github.com>
2023-03-07 14:48:53 -0500

    final

--- lab4.cpp
+++ lab4.cpp
@@ -36,12 +36,14 @@ void isolate(double number, int& integer_part, double& decimal_part); //Has 3 pa
 void display(char sign, int integer_part, double decimal_part, string integer_binary); //display sign and exponent part
 void inttable(int integer_part, string& integer_binary, int bits); //convert int part to binary
 void toBinaryDec(double decimal_part, string& decimaltobinary); //convert decimal part to binary
-void binRepresenation(string& full_binary, string whole, string dec); //concats whole and fraction portion
+void binRepresentation(string& full_binary, string whole, string dec); //concats whole and fraction portion
 void getMantissa(string& mantissa, string full_binary, int& shifts); //gets matissa and shifts
 void getExponent(string& exponent, int shifts); //get exponent
 void ieeeDouble(char sign, string exponent, string mantissa, string& number_double); //combine sign, exponent, mantissa
 void toHex(string& hex_string, string doubleString); //convert double to hex
 void writeToFile(values_t values);
+void display(values_t values);
+
 int main() {
     values_t values;
     //declarations/ initialization
@@ -56,60 +58,13 @@ int main() {
     isolate(values.number, values.integer_part, values.decimal_part);
     inttable(values.integer_part, values.integer_binary, (int)log2(abs(values.number))+1);
     toBinaryDec(values.decimal_part, values.decimal_binary);
-    binRepresenation(values.full_binary, values.integer_binary, values.decimal_binary);
+    binRepresentation(values.full_binary, values.integer_binary, values.decimal_binary);
     getMantissa(values.mantissa, values.full_binary, values.shifts);
     getExponent(values.exponent, values.shifts);
     ieeeDouble(values.sign, values.exponent, values.mantissa, values.number_double);
     toHex(values.hex_string, values.number_double);
+    display(values);
     
-    cout << "Please choose one of the following operations: " << endl;
-    cout << "1. DISPLAY THE SIGN BIT VALUE " << endl;
-    cout << "2. DISPLAY THE INTEGER PART IN BOTH BASE-10 AND BINARY FORMATS" << endl;
-    cout << "3. DISPLAY THE DECIMAL PART IN BOTH BASE-10 AND BINARY FORMATS" << endl;
-    cout << "4. DISPLAY THE NUMBER ENTERED IN BOTH BASE-10 AND BINARY FORMATS" << endl;
-    cout << "5. DISPLAY THE MANTISSA IN BINARY FORMAT" << endl;
-    cout << "6. DISPLAY THE EXPONENT IN BOTH BASE-10 AND BINARY FORMAT" << endl;
-    cout << "7. DISPLAY THE IEEE 754 DOUBLE PRECISION BINARY LAYOUT" << endl;
-    cout << "8. DISPLAY THE IEEE 754 DOUBLE PRECISION HEX LAYOUT" << endl;
-    cout << "9. SEND THE DETAILED SOLUTION TO AN EXTERNAL FILE" << endl;
-    int selection = 1;
-    while(selection != 0)
-    {
-        cout << "enter selection(0 to quit): ";
-        cin >> selection;
-        switch(selection)
-        {          
-            case 1:
-                cout << values.sign << endl;
-                break;
-            case 2:
-                cout << values.integer_part << " " << values.integer_binary << endl;
-                break;
-            case 3:
-                cout << values.decimal_part << " " << values.decimal_binary << endl;
-                break;
-            case 4:
-                cout << values.number << " " << values.full_binary << endl;
-                break;
-            case 5:
-                cout <<values.mantissa << endl;
-                break;
-            case 6:
-                cout << 1023+values.shifts << " " << values.exponent << endl;
-                break;
-            case 7:
-                cout << values.number_double << endl;
-                break;
-            case 8:
-                cout << values.hex_string << endl;
-                break;
-            case 9:
-                writeToFile(values);
-            default:
-                return 0;
-        }
-    }
-    return 0;
 }
 
 // communicating by pass by reference
@@ -158,11 +113,10 @@ void toBinaryDec(double decimal_part, string& decimaltobinary) {
         decimaltobinary += to_string(intpart);
 
     }
-    cout << decimaltobinary;
 }
 
 //obtain binary represenation of user input
-void binRepresenation(string& full_binary, string whole, string dec)
+void binRepresentation(string& full_binary, string whole, string dec)
 {
     full_binary = whole + "." + dec;
 }
@@ -247,4 +201,54 @@ void writeToFile(values_t values) {
     myfile << "IEEE754 Double Binary: " << values.number_double << endl;
     myfile << "IEEE754 Hex: " << values.hex_string << endl;
     myfile.close();
+}
+
+void display(values_t values) {
+    cout << "Please choose one of the following operations: " << endl;
+    cout << "1. DISPLAY THE SIGN BIT VALUE " << endl;
+    cout << "2. DISPLAY THE INTEGER PART IN BOTH BASE-10 AND BINARY FORMATS" << endl;
+    cout << "3. DISPLAY THE DECIMAL PART IN BOTH BASE-10 AND BINARY FORMATS" << endl;
+    cout << "4. DISPLAY THE NUMBER ENTERED IN BOTH BASE-10 AND BINARY FORMATS" << endl;
+    cout << "5. DISPLAY THE MANTISSA IN BINARY FORMAT" << endl;
+    cout << "6. DISPLAY THE EXPONENT IN BOTH BASE-10 AND BINARY FORMAT" << endl;
+    cout << "7. DISPLAY THE IEEE 754 DOUBLE PRECISION BINARY LAYOUT" << endl;
+    cout << "8. DISPLAY THE IEEE 754 DOUBLE PRECISION HEX LAYOUT" << endl;
+    cout << "9. SEND THE DETAILED SOLUTION TO AN EXTERNAL FILE" << endl;
+    int selection = 1;
+    while(selection != 0)
+    {
+        cout << "enter selection(0 to quit): ";
+        cin >> selection;
+        switch(selection)
+        {          
+            case 1:
+                cout << values.sign << endl;
+                break;
+            case 2:
+                cout << values.integer_part << " " << values.integer_binary << endl;
+                break;
+            case 3:
+                cout << values.decimal_part << " " << values.decimal_binary << endl;
+                break;
+            case 4:
+                cout << values.integer_part << to_string(values.decimal_part).substr(1) << " " << values.full_binary << endl;
+                break;
+            case 5:
+                cout <<values.mantissa << endl;
+                break;
+            case 6:
+                cout << 1023+values.shifts << " " << values.exponent << endl;
+                break;
+            case 7:
+                cout << values.number_double << endl;
+                break;
+            case 8:
+                cout << values.hex_string << endl;
+                break;
+            case 9:
+                writeToFile(values);
+            default:
+                break;
+        }
+    }
 }
